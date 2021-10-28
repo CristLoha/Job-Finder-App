f06057e29b42b7c5fd6e002a1781d5ec8fe975bf
Christ Henry Loha <cristloha2205@gmail.com>
2021-10-28 17:52:43 +0800

    update

--- lib/widgets/recommended_card.dart
+++ lib/widgets/recommended_card.dart
@@ -6,85 +6,58 @@ class RecommendedCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 234,
-      width: 314,
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: kGreyColor,
-          width: 1.5,
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, '/detail-page'),
+      child: Container(
+        height: 234,
+        width: 314,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: kGreyColor,
+            width: 1.5,
+          ),
+          borderRadius: BorderRadius.circular(6),
+          color: Color(
+            0xffFCFCFD,
+          ),
         ),
-        borderRadius: BorderRadius.circular(6),
-        color: Color(
-          0xffFCFCFD,
-        ),
-      ),
-      child: Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 10.5),
-                  child: Image.asset(
-                    'assets/icon_company_logo.png',
-                    width: 70,
+        child: Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 10.5),
+                    child: Image.asset(
+                      'assets/icon_company_logo.png',
+                      width: 70,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Image.asset(
-                    'assets/icon_favorite.png',
-                    width: 26,
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset(
+                      'assets/icon_favorite.png',
+                      width: 26,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 14),
-            Text(
-              'Senior Product Designer',
-              style: primaryTextStyle.copyWith(fontSize: 18, fontWeight: bold),
-            ),
-            Row(
-              children: [
-                Text(
-                  'Google LLC',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 16, fontWeight: semiBold),
-                ),
-                SizedBox(width: 7),
-                Image.asset(
-                  'assets/divider.png',
-                  width: 4,
-                ),
-                SizedBox(width: 7),
-                Text(
-                  'South Jakarta',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 16, fontWeight: semiBold),
-                ),
-              ],
-            ),
-            Spacer(),
-            Expanded(
-              child: Row(
+                ],
+              ),
+              SizedBox(height: 14),
+              Text(
+                'Senior Product Designer',
+                style:
+                    primaryTextStyle.copyWith(fontSize: 18, fontWeight: bold),
+              ),
+              Row(
                 children: [
                   Text(
-                    '10 June 2021',
-                    style: greyTextStyle.copyWith(
-                        fontSize: 16, fontWeight: semiBold),
-                  ),
-                  SizedBox(width: 50),
-                  Text(
-                    'Onsite',
+                    'Google LLC',
                     style: primaryTextStyle.copyWith(
-                      fontSize: 16,
-                      fontWeight: semiBold,
-                    ),
+                        fontSize: 16, fontWeight: semiBold),
                   ),
                   SizedBox(width: 7),
                   Image.asset(
@@ -92,17 +65,48 @@ class RecommendedCard extends StatelessWidget {
                     width: 4,
                   ),
                   SizedBox(width: 7),
-                  Expanded(
-                    child: Text(
-                      '\$8k/mo',
-                      style: primaryTextStyle.copyWith(
-                          fontSize: 16, fontWeight: semiBold),
-                    ),
+                  Text(
+                    'South Jakarta',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 16, fontWeight: semiBold),
                   ),
                 ],
               ),
-            )
-          ],
+              Spacer(),
+              Expanded(
+                child: Row(
+                  children: [
+                    Text(
+                      '10 June 2021',
+                      style: greyTextStyle.copyWith(
+                          fontSize: 16, fontWeight: semiBold),
+                    ),
+                    SizedBox(width: 50),
+                    Text(
+                      'Onsite',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    SizedBox(width: 7),
+                    Image.asset(
+                      'assets/divider.png',
+                      width: 4,
+                    ),
+                    SizedBox(width: 7),
+                    Expanded(
+                      child: Text(
+                        '\$8k/mo',
+                        style: primaryTextStyle.copyWith(
+                            fontSize: 16, fontWeight: semiBold),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
